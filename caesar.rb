46384348c88fbab6457606bdfdf8f2c60eec59b9
Stacey Muscat <staceymuscat@gmail.com>
2021-03-16 13:31:45 +1100

    Create base method and calls to method

---
+++ caesar.rb
@@ -0,0 +1,10 @@
+def caesar_cipher(string, shift)
+
+end
+
+
+caesar_cipher("Aa", 5)
+caesar_cipher("Zz", 5)
+caesar_cipher("Hello", 5)
+caesar_cipher("Hello World", 5)
+caesar_cipher("Hello World!", 5)
