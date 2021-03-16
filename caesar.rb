0e7c6bb7cc8d8463727703ae9d2535e7f9c721b5
Stacey Muscat <staceymuscat@gmail.com>
2021-03-16 13:35:26 +1100

    Create method that works with tests

--- caesar.rb
+++ caesar.rb
@@ -1,5 +1,15 @@
 def caesar_cipher(string, shift)
 
+  cipher = ''
+
+  string.each_char do |letter|
+    if ('a'..'z').include? (letter.downcase)
+      shift.times { letter = letter.next}
+    end
+    cipher << letter[-1]
+  end
+  return cipher
+
 end
 
 
